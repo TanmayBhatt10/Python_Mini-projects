500ffeea28e1757984b41c38ef13b61ab91bc077
TanmayBhatt10 <bhatttanmay2110@gmail.com>
2025-06-19 11:38:05 +0530

    Update car_manager.py

--- turtle-crossing-start (1)/car_manager.py
+++ turtle-crossing-start (1)/car_manager.py
@@ -1,7 +1,31 @@
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
+from turtle import Turtle
+import random
 
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple","cyan","pink"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 5
+CORDS = [20,50,100,150,-45,-90,-150,-20]
 
 class CarManager:
-    pass
+
+    def __init__(self):
+        self.all_cars = []
+        self.move_speed = STARTING_MOVE_DISTANCE
+
+    def create_cars(self):
+        random_chance = random.randint(1,6)
+        if random_chance == 1:
+           new_car = Turtle('square')
+           new_car.shapesize(1,2)
+           new_car.penup()
+           new_car.color(random.choice(COLORS))
+           random_y = random.randint(-250,250)
+           new_car.goto(300,random_y)
+           self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for i in self.all_cars:
+            i.backward(self.move_speed)
+
+    def level_up(self):
+        self.move_speed += MOVE_INCREMENT
